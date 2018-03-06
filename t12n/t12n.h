dcc0118847448e4b6d1ba454725c5affa520f54c
Alejandro Isaza <al@isaza.ca>
2018-03-06 10:31:19 -0800

    Initial Commit

---
+++ t12n/t12n.h
@@ -0,0 +1,19 @@
+//
+//  t12n.h
+//  t12n
+//
+//  Created by Alejandro Isaza on 2018-03-06.
+//  Copyright © 2018 Alejandro Isaza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for t12n.
+FOUNDATION_EXPORT double t12nVersionNumber;
+
+//! Project version string for t12n.
+FOUNDATION_EXPORT const unsigned char t12nVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <t12n/PublicHeader.h>
+
+
